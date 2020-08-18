5936835436d218d11053a3b9509881e5ff52a1d2
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-18 12:05:22 +0600

    simple javascript

---
+++ me.js
@@ -0,0 +1,32 @@
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script>
