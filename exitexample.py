92999a8c7b2193929f5b3ca5aeafb792c8d6e9b0
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-19 02:20:00 +0600

    one-of -first -project

---
+++ exitexample.py
@@ -0,0 +1,9 @@
+import sys
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+        print('you typed ' + 'response' + '.')
+    else:
+        print('ki khojo tumi ?')
