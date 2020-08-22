88536ba3a4de4bf93eb9ff437c39790ebb2be61c
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-22 20:20:03 +0600

    additedviewsfile

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from second_app.models import Musician, Album
-from second_app import forms
+from first_app.models import Musician, Album
+from first_app import forms
 from django.db.models import Avg,Max
 
 # Create your views from here.
@@ -10,7 +10,18 @@ from django.db.models import Avg,Max
 def index(request):
     musician_list = Musician.objects.order_by('first_name')
     diction = {'title':"Home Page",'musician_list':musician_list}
-    return render(request,'second_app/index.html',context=diction )
+    return render(request,'first_app/index.html',context=diction )
+
+
+
+
+
+
+
+
+
+
+
 
 
 def album_list(request,artist_id):
@@ -18,11 +29,20 @@ def album_list(request,artist_id):
     album_list = Album.objects.filter(artist=artist_id).order_by('name','release_date')
     artist_rating = Album.objects.filter(artist=artist_id).aggregate(Avg('num_stars'))
 
-
-
     diction = {'title':"List of Albums",'artist_info':artist_info,'album_list':album_list,
     'artist_rating':artist_rating}
-    return render(request,'second_app/album_list.html',context=diction )
+    return render(request,'first_app/album_list.html',context=diction )
+
+
+
+
+
+
+
+
+
+
+
 
 
 def musician_form(request):
@@ -37,7 +57,21 @@ def musician_form(request):
 
 
     diction = {'title':"Add Musician",'musician_form':form}
-    return render(request,'second_app/musician_form.html',context=diction )
+    return render(request,'first_app/musician_form.html',context=diction )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def album_form(request):
@@ -50,7 +84,20 @@ def album_form(request):
             return index(request)
 
     diction = {'title':"Add Album",'album_form':form}
-    return render(request,'second_app/album_form.html',context=diction )
+    return render(request,'first_app/album_form.html',context=diction )
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -67,7 +114,20 @@ def edit_artist(request,artist_id):
 
 
     diction ={'edit_form':form}
-    return render(request, 'second_app/edit_artist.html', context=diction)
+    return render(request, 'first_app/edit_artist.html', context=diction)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -87,7 +147,21 @@ def edit_album(request,album_id):
 
     diction.update({'edit_form':form})
     diction.update({'album_id':album_id})
-    return render(request,'second_app/edit_album.html',context=diction)
+    return render(request,'first_app/edit_album.html',context=diction)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -95,11 +169,22 @@ def delete_album(request,album_id):
     album = Album.objects.get(pk=album_id).delete()
     diction = {'delete_success':'Album deleted successfully!'}
 
-    return render(request, 'second_app/delete.html',context=diction)
+    return render(request, 'first_app/delete.html',context=diction)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 def delete_musician (request,artist_id):
     artist = Musician.objects.get(pk=artist_id).delete(0)
     diction = {'delete_success':'Musician deleted successfully!'}
-    return render(request,'second_app/delete.html', context=diction)
+    return render(request,'first_app/delete.html', context=diction)
