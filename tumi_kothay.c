fb2765813d907db2c8e10d5fef7067e7440ff8f6
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-22 20:14:10 +0600

    firstcfile

---
+++ tumi_kothay.c
@@ -0,0 +1,91 @@
+int main()
+{
+    printf("Hello, World!\n");
+    printf("Hello, i am joy!\n");
+    printf("Hello, i forget c programming !\n");
+    printf("Hello, do you accept me as your lover !\n");
+    printf("Hello, or you rejected me for this!\n");
+    printf("Hello, you are stayed in my heart !\n");
+    getch(); //Use to get one character input from user, and it will not be printed on screen.
+    return 0;
+}
+
+
+
+int main (void)
+{
+   FILE *fileName;
+   int i=2, j=3, k=4, n;
+   fileName = fopen ("anything.c","w");
+   putw(i, fileName);
+   putw(j, fileName);
+   putw(k, fileName);
+   fclose(fileName);
+
+   fileName = fopen ("test.c","r");
+   while(getw(fileName)! = EOF)
+   {
+      n= getw(fileName);
+      printf("Value is %d \t: ", n);
+   }
+   fclose(fp);
+   return 0;
+}
+
+int main (void)
+{
+   FILE *fileName;
+   int i=2, j=3, k=4, n;
+   fileName = fopen ("anything.c","w");
+   putw(i, fileName);
+   putw(j, fileName);
+   putw(k, fileName);
+   fclose(fileName);
+
+   fileName = fopen ("test.c","r");
+   while(getw(fileName)! = EOF)
+   {
+      n= getw(fileName);
+      printf("Value is %d \t: ", n);
+   }
+   fclose(fp);
+   return 0;
+}
+int main (void)
+{
+   FILE *fileName;
+   int i=2, j=3, k=4, n;
+   fileName = fopen ("anything.c","w");
+   putw(i, fileName);
+   putw(j, fileName);
+   putw(k, fileName);
+   fclose(fileName);
+
+   fileName = fopen ("test.c","r");
+   while(getw(fileName)! = EOF)
+   {
+      n= getw(fileName);
+      printf("Value is %d \t: ", n);
+   }
+   fclose(fp);
+   return 0;
+}
+int main (void)
+{
+   FILE *fileName;
+   int i=2, j=3, k=4, n;
+   fileName = fopen ("anything.c","w");
+   putw(i, fileName);
+   putw(j, fileName);
+   putw(k, fileName);
+   fclose(fileName);
+
+   fileName = fopen ("test.c","r");
+   while(getw(fileName)! = EOF)
+   {
+      n= getw(fileName);
+      printf("Value is %d \t: ", n);
+   }
+   fclose(fp);
+   return 0;
+}
