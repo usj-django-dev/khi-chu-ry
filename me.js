6144b5367975f9883b7f49a0b26cddd49a5075d7
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-23 02:47:32 +0600

    editedjavascriptfile

--- me.js
+++ me.js
@@ -29,4 +29,165 @@ while (i < 10);
 <script>
 document.getElementById("demo").innerHTML =
 obj.employees[1].firstName + " " + obj.employees[1].lastName;
-</script>
+</script
+
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
