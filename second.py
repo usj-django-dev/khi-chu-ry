7f2eb84c846ab2b7460c0cb16f12ba5f10735ac0
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-25 23:56:27 +0600

    rftghjmk,mn

---
+++ second.py
@@ -0,0 +1,15 @@
+print('What is your name?')
+name = input()
+if name == 'Mary':
+    print('Helo Mary')
+print('Write down the password?')
+password = input()
+
+if password == 'swordfish':
+    print('Access granted. ')
+else:
+    print('Wrong password')
+
+
+
+
