ec8894db64f7d4d845ec30e6f3df2af022c8fa31
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-25 23:55:41 +0600

    twofiles

---
+++ First-Programme.py
@@ -0,0 +1,8 @@
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
