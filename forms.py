da69ef0e44e9e4bd5c8978406248391e08485a9f
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-26 22:01:19 +0600

    formfromfoiiu

--- forms.py
+++ forms.py
@@ -1,13 +1,9 @@
 from django import forms
+from django.core import validators
 from first_app import models
 
-class Musician_Form(forms.ModelForm):
+
+class MusicianForm(forms.ModelForm):
     class Meta:
         model = models.Musician
-        fields = "__all__"
-
-class Album_Form(forms.ModelForm):
-    release_date = forms.DateTimeField(widget=forms.TextInput(attrs={'type':'date'}))
-    class Meta:
-        model = models.Album
         fields = "__all__"
