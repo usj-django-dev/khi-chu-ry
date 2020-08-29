31cd6ef4b3b753b7d32d257d4e6f650208b12226
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-08-29 23:04:12 +0600

    Add files via upload

---
+++ vybujn.vb
@@ -0,0 +1,33 @@
+var provider = new PhysicalFileProvider(applicationRoot);
+var contents = provider.GetDirectoryContents(string.Empty);
+var filePath = Path.Combine("wwwroot", "js", "site.js");
+var fileInfo = provider.GetFileInfo(filePath);
+Types in the preceding example:
+
+provider is an IFileProvider.
+contents is an IDirectoryContents.
+fileInfo is an IFileInfo.var provider = new PhysicalFileProvider(applicationRoot);
+var contents = provider.GetDirectoryContents(string.Empty);
+var filePath = Path.Combine("wwwroot", "js", "site.js");
+var fileInfo = provider.GetFileInfo(filePath);
+Types in the preceding example:
+
+provider is an IFileProvider.
+contents is an IDirectoryContents.
+fileInfo is an IFileInfo.var provider = new PhysicalFileProvider(applicationRoot);
+var contents = provider.GetDirectoryContents(string.Empty);
+var filePath = Path.Combine("wwwroot", "js", "site.js");
+var fileInfo = provider.GetFileInfo(filePath);
+Types in the preceding example:
+
+provider is an IFileProvider.
+contents is an IDirectoryContents.
+fileInfo is an IFileInfo.var provider = new PhysicalFileProvider(applicationRoot);
+var contents = provider.GetDirectoryContents(string.Empty);
+var filePath = Path.Combine("wwwroot", "js", "site.js");
+var fileInfo = provider.GetFileInfo(filePath);
+Types in the preceding example:
+
+provider is an IFileProvider.
+contents is an IDirectoryContents.
+fileInfo is an IFileInfo.
