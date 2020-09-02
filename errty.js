c41529c9dc2db2c4285732f455529072acd8aca1
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-02 19:41:58 +0600

    eertrtyugv

---
+++ errty.js
@@ -0,0 +1,193 @@
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+
+
+
+
+
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"utsho_sadhak", "lastName":"joy"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+
+
+
+
+
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script
