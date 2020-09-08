60cfb58b32a9f02f80919a4c41c9372a0225fdf1
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-08 22:57:17 +0600

    piuyhserg

--- models.py
+++ models.py
@@ -1,32 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Musician(models.Model):
-    #id = models.AutoFiled(Primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=50)
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+
+    facebook_id = models.URLField(blank=True)
+
+
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
-
-
-class Album(models.Model):
-    #id = models.AutoFiled(Primary_key=True)
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
-
-    rating = (
-    (1,  "Worst"),
-    (2,  "Bad"),
-    (3,  "Not Bad"),
-    (4,  "Good"),
-    (5,  "Excelient!"),
-    )
-
-    num_stars = models.IntegerField(choices=rating)
-    def __str__(self):
-        return self.name + ", Rating: " + str(self.num_stars)
+        return self.user.username
