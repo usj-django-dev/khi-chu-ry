04d55046d329906ccdc69d608280ae08e11ad349
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-10 23:11:21 +0600

    fhbhgfj

--- forms.py
+++ forms.py
@@ -1,17 +1,9 @@
 from django import forms
-from django.contrib.auth.models import User
-from Login_app.models import UserInfo
+from django.core import validators
+from first_app import models
 
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+class MusicianForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ('username','password','email')
-
-
-
-class UserInfoForm(forms.ModelForm):
-    class Meta():
-        model = UserInfo
-        fields = ('facebook_id','profile_pic')
+        model = models.Musician
+        fields = "__all__"
