fe120e10874cd87027871a561d27d71af5564afb
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-25 02:56:24 +0600

    eeewyueyjh
    eyjgn

--- 1_app.py
+++ 1_app.py
@@ -2,3 +2,137 @@ print("Utsho Sadhak Joy")
 print('O----')
 print(' ||||')
 print('*'* 10)
+    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>
+    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>
