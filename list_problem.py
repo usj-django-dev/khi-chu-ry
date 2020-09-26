5aa0c2f7ef4d1f12851ae4109106514cb785eab1
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-26 23:31:17 +0600

    styj
    jke6t

---
+++ list_problem.py
@@ -0,0 +1,36 @@
+T = input('write down the Test_Case number , T = ')
+num = int(T)
+counter = 0
+while counter < num:
+   s = input("")
+   counter += 1
+else:
+    lst = []
+    n = int(input(" "))
+    for i in range(0, n):
+        ele = int(input())
+
+        lst.append(ele)  # adding the element
+
+    print(lst)
+    # l = ['']
+    # return_value = l.pop()
+    # print(" List Problem ")
+
+
+# s = input("ENTER A SENTENCE PROPERLY ::")
+
+
+# creating an empty list
+lst = []
+
+# number of elemetns as input
+n = int(input("Enter number of elements : "))
+
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+
+    lst.append(ele)  # adding the element
+
+print(lst)
