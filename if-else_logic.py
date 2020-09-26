321deabcbb4d6e1f2ade9af1716a4f8a2cf596d2
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-26 23:32:28 +0600

    uh3w5jwn
    e6u8lk,m

---
+++ if-else_logic.py
@@ -0,0 +1,74 @@
+T = input ('Write down the Test_Case number, T = ')
+
+num = int(T)
+
+if num <= 100:
+    pass
+else:
+    print(" babu-kahicho")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if num <= 100:
+#     print("fuck")
+#     counter = 0
+#     while counter <= num:
+#
+#     count += 1
+# else:
+#     print("The number of test case will be less than or equal 100 ")
+
