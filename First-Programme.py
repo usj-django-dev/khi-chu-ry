fcd533f8cf6b5c999d26579bf5fcfcdd7c014f56
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-26 23:34:02 +0600

    tynhseyrhn
    serhm é

--- First-Programme.py
+++ First-Programme.py
@@ -14,7 +14,132 @@ print('You will be ' + str(int(myAge) + 1) + ' in a year.')
 
         return a + b;
     }
+T = input ('Write down the Test_Case number, T = ')
 
+num = int (T)
+
+counter = 0
+
+while counter <= num:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Inside else")
+# def reverseword(s):
+#     w = s.split(" ")
+#     nw = [i[::-1] for i in w]
+#     ns = " ".join(nw)
+#     return ns
+#
+# s = input("")
+# print(reverseword(s))
+
+
+
+
+
+
+T = input ('Write down the Test_Case number, T = ')
+
+num = int (T)
+
+counter = 0
+
+while counter <= num:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Inside else")
+# def reverseword(s):
+#     w = s.split(" ")
+#     nw = [i[::-1] for i in w]
+#     ns = " ".join(nw)
+#     return ns
+#
+# s = input("")
+# print(reverseword(s))
+
+
+
+
+
+
+T = input ('Write down the Test_Case number, T = ')
+
+num = int (T)
+
+counter = 0
+
+while counter <= num:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Inside else")
+# def reverseword(s):
+#     w = s.split(" ")
+#     nw = [i[::-1] for i in w]
+#     ns = " ".join(nw)
+#     return ns
+#
+# s = input("")
+# print(reverseword(s))
+
+
+
+
+
+
+T = input ('Write down the Test_Case number, T = ')
+
+num = int (T)
+
+counter = 0
+
+while counter <= num:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Inside else")
+# def reverseword(s):
+#     w = s.split(" ")
+#     nw = [i[::-1] for i in w]
+#     ns = " ".join(nw)
+#     return ns
+#
+# s = input("")
+# print(reverseword(s))
+
+
+
+
+
+
+T = input ('Write down the Test_Case number, T = ')
+
+num = int (T)
+
+counter = 0
+
+while counter <= num:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Inside else")
+# def reverseword(s):
+#     w = s.split(" ")
+#     nw = [i[::-1] for i in w]
+#     ns = " ".join(nw)
+#     return ns
+#
+# s = input("")
+# print(reverseword(s))
+
+
+
+
+
+
+e
     /// <summary>
     /// Divides an integer by another and returns the result.
     /// </summary>
