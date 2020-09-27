b85c91881a61701a39960fcee6ea014dbca6f36b
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-27 20:13:05 +0600

    zetrxdyctfvygukhbml;,
    3q%Zwxrycdtufvygbkhjl;

---
+++ unboxing.py
@@ -0,0 +1,16 @@
+lst = []
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    ele = str(input())
+    lst.append(ele)
+# print(lst)
+
+for  
+
+# Python program to print list
+# using for loop
+# a = [1, 2, 3, 4, 5]
+
+# printing the list using loop
+for i in range(len(lst)):
+    print (lst[x])
