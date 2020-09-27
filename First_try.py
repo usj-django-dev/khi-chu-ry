f6eae0a938bf34e5ed8af444b303f271197424b7
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-09-27 20:11:04 +0600

    erxtdcfvbh
    4wesrdtfgh
    fgcvbnm
    fcgvhbn

---
+++ First_try.py
@@ -0,0 +1,54 @@
+T = int(input("Write the test case number, T =  "))
+if T <= 100:
+    lst = []
+    # n = int(input("Enter number of elements : "))
+    for i in range(0, T):
+        ele = str(input())
+        lst.append(ele)
+    print(lst)
+
+else:
+    print("The test case number will be less than 100")
+
+
+#
+# while number < 5 :
+#     print("Thank you")
+#     # Increment the value of the variable "number by 1"
+#     number = number+1
+
+
+
+
+
+#
+# counter = 0
+#
+# while counter < T:
+#     print("Inside loop")
+#     counter = counter + 1
+# else:
+#     print("Inside else")
+
+
+
+
+
+
+
+
+# first success:
+# T = int(input("Write the test case number, T =  "))
+# if T <= 100:
+#     lst = []
+#     # n = int(input("Enter number of elements : "))
+#     for i in range(0, T):
+#         ele = str(input())
+#         lst.append(ele)
+#     print(lst)
+
+# else:
+#     print("The test case number will be less than 100")
+
+
+
