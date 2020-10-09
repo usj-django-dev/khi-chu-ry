23b00afbc064c770443872bec3185d6a9e1db793
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-10-09 15:23:21 +0600

    qqqqqqqqqqqqqqqqq
    45ygjnrs

--- list_problem.py
+++ list_problem.py
@@ -22,15 +22,15 @@ else:
 
 
 # creating an empty list
-lst = []
+# lst = []
 
 # number of elemetns as input
-n = int(input("Enter number of elements : "))
+# n = int(input("Enter number of elements : "))
 
 # iterating till the range
-for i in range(0, n):
-    ele = int(input())
-
-    lst.append(ele)  # adding the element
-
-print(lst)
+# for i in range(0, n):
+#     ele = int(input())
+#
+#     lst.append(ele)  # adding the element
+#
+# print(lst)
