61c7f0ac02a944ccbdd530e0d7c73ebdf068bd53
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-10-20 22:44:01 +0600

    aetjaethe
    aetjae

--- urls.py
+++ urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-from API_examples import views
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('account/', include('App_Login.urls')),
+    path('blog/', include('App_Blog.urls')),
+    path('', views.index, name='index'),
 ]
+
+
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
