59bdcccf4f3e4cc6caefb0d837836042724071f4
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-11-04 04:23:13 +0600

    yujh
    jyhtf

---
+++ rheeb.py
@@ -0,0 +1,325 @@
+public static int Add(int a, int b)
+    {
+        if ((a == int.MaxValue && b > 0) || (b == int.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Adds two doubles and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The sum of two doubles.
+    /// </returns>
+    /// <exception cref="System.OverflowException">Thrown when one parameter is max
+    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,authenticate,logout
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+# Create your views here.
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    registered = False
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            registered = True
+    dict = {'form':form, 'registered':registered}
+    return render(request, 'App_Login/signup.html', context = dict)
+
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.clean_data.get('username')
+            password = form.clean_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+    return render(request,'App_Login/login.html',context={'form':form})
+sdfr
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>
+    <members name="math">
+        <Math>
+            <summary>
+            The main <c>Math</c> class.
+            Contains all methods for performing basic math functions.
+            </summary>
+            <remarks>
+            <para>This class can add, subtract, multiply and divide.</para>
+            <para>These operations can be performed on both integers and doubles.</para>
+            </remarks>
+        </Math>
+        <AddInt>
+            <summary>
+            Adds two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+            </summary>
+            <returns>
+            The sum of two integers.
+            </returns>
+            <example>
+            <code>
+            int c = Math.Add(4, 5);
+            if (c > 10)
+            {
+                Console.WriteLine(c);
+            }
+            </code>
+            </example>
+            <exception cref="System.OverflowException">Thrown when one parameter is max
+            and the other is greater than 0.</exception>
+            See <see cref="Math.Add(double, double)"/> to add doubles.
+            <seealso cref="Math.Subtract(int, int)"/>
+            <seealso cref="Math.Multiply(int, int)"/>
+            <seealso cref="Math.Divide(int, int)"/>
+            <param name="a">An integer.</param>
+            <param name="b">An integer.</param>
+        </AddInt>
+        <AddDouble>
+            <summary>
+            Adds two doubles <paramref name="a"/> and <paramref name="b"/> and returns the result.
+            </summary>
+            <returns>
+            The sum of two doubles.
+            </returns>
+            <example>
+            <code>
+            double c = Math.Add(4.5, 5.4);
+            if (c > 10)
+            {
+                Console.WriteLine(c);
+            }
+            </code>
+            </example>
+            <exception cref="System.OverflowException">Thrown when one parameter is max
+            and the other is greater than 0.</exception>
+            See <see cref="Math.Add(int, int)"/> to add integers.
+            <seealso cref="Math.Subtract(double, double)"/>
+            <seealso cref="Math.Multiply(double, double)"/>
+            <seealso cref="Math.Divide(double, double)"/>
+            <param name="a">A double precision number.</param>
+            <param name="b">A double precision number.</param>
+        </AddDouble>
+        <SubtractInt>
+            <summary>
+            Subtracts <paramref name="b"/> from <paramref name="a"/> and returns the result.
+            </summary>
+            <returns>
+            The difference between two integers.
+            </returns>
+            <example>
+            <code>
+            int c = Math.Subtract(4, 5);
+            if (c > 1)
+            {
+                Console.WriteLine(c);
+            }    /// and the other is greater than zero.</exception>
+    public static double Add(double a, double b)
+    {
+        if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+            throw new System.OverflowException();
+
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides an integer by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two integers.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    /// <summary>
+    /// Divides a double by another and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The division of two doubles.
+    /// </returns>
+    /// <exception cref="System.DivideByZeroException">Thrown when a division by zero occurs.</exception>
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
+<docs>
+            </code>
+            </example>
+            See <see cref="Math.Subtract(double, double)"/> to subtract doubles.
+            <seealso cref="Math.Add(int, int)"/>
+            <seealso cref="Math.Multiply(int, int)"/>
+            <seealso cref="Math.Divide(int, int)"/>
+            <param name="a">An integer.</param>
+            <param name="b">An integer.</param>
+        </SubtractInt>
+        <SubtractDouble>
+            <summary>
+            Subtracts a double <paramref name="b"/> from another double <paramref name="a"/> and returns the result.
+            </summary>
+            <returns>
+            The difference between two doubles.
+            </returns>
+            <example>
+            <code>
+            double c = Math.Subtract(4.5, 5.4);
+            if (c > 1)
+            {
+                Console.WriteLine(c);
+            }
+            </code>
+            </example>
+            See <see cref="Math.Subtract(int, int)"/> to subtract integers.
+            <seealso cref="Math.Add(double, double)"/>
+            <seealso cref="Math.Multiply(double, double)"/>
+            <seealso cref="Math.Divide(double, double)"/>
+            <param name="a">A double precision number.</param>
+            <param name="b">A double precision number.</param>
+        </SubtractDouble>
+        <MultiplyInt>
+            <summary>
+            Multiplies two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+            </summary>
+            <returns>
+            The product of two integers.
+            </returns>
+            <example>
+            <code>
+            int c = Math.Multiply(4, 5);
+            if (c > 100)
+            {
+                Console.WriteLine(c);
+            }
+            </code>
+            </example>
+            See <see cref="Math.Multiply(double, double)"/> to multiply doubles.
+            <seealso cref="Math.Add(int, int)"/>
+            <seealso cref="Math.Subtract(int, int)"/>
+            <seealso cref="Math.Divide(int, int)"/>
+            <param name="a">An integer.</param>
+            <param name="b">An integer.</param>
+        </MultiplyInt>
+        <MultiplyDouble>
+            <summary>
+            Multiplies two doubles <paramref name="a"/> and <paramref name="b"/> and returns the result.
+            </summary>
+            <returns>
+            The product of two doubles.
+            </returns>
+            <example>
+            <code>
+            double c = Math.Multiply(4.5, 5.4);
+            if (c > 100.0)
+            {
+                Console.WriteLine(c);
+            }
+            </code>
+            </example>
+            See <see cref="Math.Multiply(int, int)"/> to multiply integers.
+            <seealso cref="Math.Add(double, double)"/>
+            <seealso cref="Math.Subtract(double, double)"/>
+            <seealso cref="Math.Divide(double, double)"/>
+            <param name="a">A double precision number.</param>
+            <param name="b">A double precision number.</param>
+        </MultiplyDouble>
+        <DivideInt>
+            <summary>
+            Divides an integer <paramref name="a"/> by another integer <paramref name="b"/> and returns the result.
+            </summary>
+            <returns>
+            The quotient of two integers.
+            </returns>
+            <example>
+            <code>
+            int c = Math.Divide(4, 5);
+            if (c > 1)
+            {
+                Console.WriteLine(c);
+            }
+            </code>
+            </example>
+            <exception cref="System.DivideByZeroException">Thrown when <paramref name="b"/> is equal to 0.</exception>
+            See <see cref="Math.Divide(double, double)"/> to divide doubles.
+            <seealso cref="Math.Add(int, int)"/>
+            <seealso cref="Math.Subtract(int, int)"/>
+            <seealso cref="Math.Multiply(int, int)"/>
+            <param name="a">An integer dividend.</param>
+            <param name="b">An integer divisor.</param>
+        </DivideInt>
+        <DivideDouble>
