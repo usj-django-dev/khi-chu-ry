5ad65191c7a549c6f9e4cf2ebcaac22a3735184f
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-11-07 20:30:21 +0600

    ybhtvg6byh
    4byhvg

--- App.js
+++ App.js
@@ -1,30 +1,76 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/welcome'
-import Hello from './components/Hello'
-function App() {
-  return (
-    <div className="App">
-      <Greet name = "Bruce" heroName="Batman"> 
-        <p>This is children props</p> 
-      </Greet>
+import Book from './components/Book';
 
-      <Greet name = "Clark" heroName="Superman"> 
-        <button>Action</button>
-      </Greet>
+class App extends Component {
+  state = {
+    books: [
+      { bookName: "1984", writer: "George Orwell" },
+      { bookName: "The Da Vinci Code", writer: "Dan Brown" },
+      { bookName: "The Alchemist", writer: "Paulo Coelho" },
+    ],
+    otherProp: "I am from other Prop"
+  }
 
-      {/* <Greet name = "Clark" heroName="Superman"/> */}
-      <Greet name = "Diana" heroName="Wounder Womans"/>
+  ChangeBookState = newBookName => {
+    this.setState({
+      books: [
+        { bookName: newBookName, writer: "George Orwell" },
+        { bookName: "The Da Vinci Code", writer: "Dan Brown" },
+        { bookName: "Metamorfosis", writer: "Franz Kafka" },
+      ],
 
-      <Welcome name = "Bruce" heroName="Batman"/>
-      <Welcome name = "Clark" heroName="Superman"/>
-      <Welcome name = "Diana" heroName="Wounder Womans"/>
+    });
+  }
 
-      {/* <Hello /> */}
-    </div>
-  );
+
+  changeWithInputState = event => {
+    this.setState({
+      books: [
+        { bookName: event.target.value, writer: "George Orwell" },
+        { bookName: "The Da Vinci Code", writer: "Dan Brown" },
+        { bookName: "Metamorfosis", writer: "Franz Kafka" },
+      ],
+
+    });
+
+  }
+
+  render() {
+    const style = {
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "white"
+    };
+    console.log(this.state)
+    return (
+      <div className="App">
+        <h1 style={style}> Book List</h1>
+        <button onClick={() => this.ChangeBookState("Nineteen-Eighty-Four")}>Change State </button>
+        <input type="text" onChange={this.changeWithInputState} ></input>
+        <Book
+          bookName={this.state.books[0].bookName}
+          writer={this.state.books[0].writer}
+          inputName={this.changeWithInputState} />
+        <Book
+          bookName={this.state.books[1].bookName}
+          writer={this.state.books[1].writer} />
+        <Book
+          bookName={this.state.books[2].bookName}
+          writer={this.state.books[2].writer}
+          change={this.ChangeBookState.bind(this, "Nineteen 84")} />
+      </div>
+    );
+
+  }
 }
+
+
+
+
+
+
+
 
 export default App;
