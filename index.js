e804c41bb8312cce68c16f1ef5f9a1c21f3f5be9
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-11-09 20:09:07 +0600

    yhnbd
    nrhtdgt

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
