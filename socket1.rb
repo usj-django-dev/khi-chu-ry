6b951ed466465f4c5949a4366c9b4050bfbba24d
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-11-26 22:09:31 +0600

    tyjhdgfb
    htgf

---
+++ socket1.rb
@@ -0,0 +1,530 @@
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   # client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   # client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   # client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Ti//.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   # client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+# server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "/ the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   # Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   # client.puts /"Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+# loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   # client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+# require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
+
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer...open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+   Thread.start(server.accept) do |client|
+   client.puts(Time.now.ctime)   # Send the time to the client
+   client.puts "Closing the connection. Bye!"
+   client.close                  # Disconnect from the client
+   end
+}
+
