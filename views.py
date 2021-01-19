8221423c100780f2af5d1f38deb84d886faa1f9d
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-19 09:14:58 +0600

    views,pty
    yf   hgfgo  uiyjh gfdcwqe erytu  9o8li kyuj yhfgdv

--- views.py
+++ views.py
@@ -1,41 +1,9 @@
-from django.shortcuts import render, HttpResponseRedirect
-from App_Login.forms import CreateNewUser
-from django.contrib.auth import authenticate, login, logout
-from django.urls import reverse, reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
-from App_Login.models import UserProfile
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.contrib.auth.decorators import login_required
+# Create your views here.
 
 
-def sign_up(request):
-    form = CreateNewUser()
-    registered = False
-    if request.method == 'POST':
-        form = CreateNewUser(request.POST)
-        if form.is_valid():
-            user = form.save()
-            registered = True
-            user_profile = UserProfile(user=user)
-            return HttpResponseRedirect(reverse('App_Login:login'))
-
-    return render(request, 'App_Login/signup.html',
-                  context={'title': 'sign up . Instagram', 'form': form})
-
-
-def login_page(request):
-    form = AuthenticationForm()
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                pass
-
-    return render(request, 'App_Login/login.html', context={'title': 'login', 'form': form})
-
-
-# afa
-# afa@gmail.com
-# afatumikoi
+@login_required
+def home(request):
+    return HttpResponse("Homepage")
