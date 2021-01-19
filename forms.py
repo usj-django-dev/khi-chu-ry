dbbc47c8f472ae1bc099a08f218e76897fc4f41d
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-19 09:20:20 +0600

    Add files via upload
    l,ikm  ujn yh tgr  vf
    ec wxs xw dfvgb  hn jmk ,lo,ikmu  jny
    hbtg v  fc xsz  sdcf vgb  hnj   mk,l,ikm  ujynhb  tgvrfc dx   szaz sxd
    cfvg b    hnj     mk,l, kmjn  hb g vfc   dx sz  azsxd cf b hnjm  k,l,k  mjn   hbgv  fcdxs  za
    Azs   dfvg  hnjkl    kjh  gfd xs  zazs   dcf   vgbh nj  mk, llkj  bg vfcdxs  zazs  xdfgb  hnjm  k,ljhg  fdsa

--- forms.py
+++ forms.py
@@ -1,25 +1,36 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from App_Login.models import UserProfile
 
 
 class CreateNewUser(UserCreationForm):
-    email = forms.EmailField(required=True, label="", widget=forms.TextInput(
-        attrs={'placeholder': 'Email'}))
-    username = forms.CharField(required=True, label="", widget=forms.TextInput(
-        attrs={'placeholder': 'username'}))
+    username = forms.CharField(required=True, label="",
+                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
 
-    password1 = forms.CharField(
-        required=True,
-        label="",
-        widget=forms.PasswordInput(attrs={'placeholder': 'password'}))
+    email = forms.EmailField(required=True, label="",
+                             widget=forms.TextInput(attrs={'placeholder': 'Email'}))
 
-    password2 = forms.CharField(
-        required=True,
-        label="",
-        widget=forms.PasswordInput(
-            attrs={'placeholder': 'password Confirmation'}))
+    password1 = forms.CharField(required=True,
+                                label="", widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+    password2 = forms.CharField(required=True,
+                                label="", widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+        }
+
+
+class EditProfile(forms.ModelForm):
+    dob = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+
+    class Meta():
+        model = UserProfile
+        exclude = ('user',)
