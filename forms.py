329bf69e6983b06685619a940239b8bcac0f6ef2
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-20 15:07:00 +0600

    ghfgd
    rtyt erber    tjklmoliu   mky   njh btrg vecwdw r wfghjk n

--- forms.py
+++ forms.py
@@ -1,36 +1,23 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from App_Login.models import UserProfile
 
 
-class CreateNewUser(UserCreationForm):
-    username = forms.CharField(required=True, label="",
-                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-
-    email = forms.EmailField(required=True, label="",
-                             widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-
-    password1 = forms.CharField(required=True,
-                                label="", widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-
-    password2 = forms.CharField(required=True,
-                                label="", widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
-
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        help_texts = {
-            'username': None,
-            'email': None,
-            'password1': None,
-            'password2': None,
-        }
 
 
-class EditProfile(forms.ModelForm):
-    dob = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+class UserProfileChange(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password')
 
-    class Meta():
+
+class ProfilePic(forms.ModelForm):
+    class Meta:
         model = UserProfile
-        exclude = ('user',)
+        fields = ['profile_pic',]
