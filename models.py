c19f78dddf204cec36014ead05043cbfd02416a6
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-26 18:53:42 +0600

    hgfdsdf
    mjnhgfdsdf

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='user_profile')
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    dob = models.DateField(auto_now_add=True, blank=True, null=True)
+    website = models.URLField(blank=True)
+    Facebook = models.URLField(blank=True)
