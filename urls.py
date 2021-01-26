c2aa9d80ad7bc54c46e745b761f1f1766225ec94
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-26 18:51:14 +0600

    i7uj
    6uy5htgrf

--- urls.py
+++ urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from App_Blog import views
-app_name = 'App_Blog'
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from App_Posts import views
+
 
 urlpatterns = [
-    path('',views.blog_list, name='blog_list'),
-    path('write/',views.CreateBlog.as_view(), name='create_blog'),
-
+    path('admin/', admin.site.urls),
+    path('accounts/', include('App_Login.urls')),
+    path('post/', include('App_Posts.urls')),
+    path('', views.home, name='home'),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
