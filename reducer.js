dde1a58124061b0cec48da525c38f413913f4865
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-28 01:52:44 +0600

    igjfybhtrg
    thrgvfc

---
+++ reducer.js
@@ -0,0 +1,70 @@
+import * as actionTypes from './actionTypes';
+
+
+const INGREDIENT_PRICES = {
+    salad: 20,
+    cheese: 40,
+    meat: 90,
+
+}
+
+
+
+
+const INITIAL_STATE = {
+    ingredients: [
+        { type: 'salad', amount: 0 },
+        { type: 'cheese', amount: 0 },
+        { type: 'meat', amount: 0 },
+
+    ],
+    totalPrice: 80,
+    purchasable: false,
+}
+
+
+export const reducer = (state = INITIAL_STATE, action) => {
+    const ingredients = [...state.ingredients];
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            for (let item of ingredients) {
+                if (item.type === action.payload) item.amount++;
+            }
+            return {
+                ...state,
+                ingredients: ingredients,
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload],
+            }
+
+        case actionTypes.REMOVE_INGREDIENT:
+            for (let item of ingredients) {
+                if (item.type === action.payload) {
+                    if (item.amount <= 0) return state;
+                    item.amount--;
+                }
+            }
+            return {
+                ...state,
+                ingredients: ingredients,
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.payload],
+            }
+
+
+        case actionTypes.UPDATE_PURCHASABLE:
+            const sum = state.ingredients.reduce((sum, element) => {
+                return sum + element.amount;
+            }, 0)
+            return {
+                ...state,
+                purchasable: sum > 0,
+            }
+
+
+        default:
+            return state;
+    }
+
+}
+
+
+
