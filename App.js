202fa89b2dd15df607ef957443ad51c77fa46e72
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-28 01:51:14 +0600

    k,jhmngbf
    yjyhgfdbty

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import MainComponent from './components/MainComponent';
+import Main from './Components/Main';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 
 function App() {
   return (
-    <div className="App">
-      <MainComponent />
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
+
+
     </div>
   );
 }
+
 
 export default App;
