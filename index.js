ef810f3fee1558ebad2f7c2f46f1a9c4fe3448be
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-28 01:52:02 +0600

    yjhtrbgf
    yujyhtgr

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
