ec066bbc7bc2dc81bb16f59f88bf75ed7f6221d7
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-30 04:02:29 +0600

    double sona

--- First_oddeven.py
+++ First_oddeven.py
@@ -14,3 +14,19 @@ if T <= 100:
 else:
     print("The test case number will be 1<=T<=100")
 
+
+T = int(input("Write the test case number, T =  "))
+if T <= 100:
+    for i in range(0, T):
+        n = int(input())
+        if 0 <= n <= 2147483647:
+            q = n % 2
+            if q > 0:
+                print("odd")
+            else:
+                print("even")
+        else:
+            print("The will be 0<=n<=2147483647")
+else:
+    print("The test case number will be 1<=T<=100")
+
