c0d0a0f693f079e018680f80f5a74d3ae1929f1d
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-02-01 17:27:16 +0600

    add

--- First_try.py
+++ First_try.py
@@ -11,3 +11,31 @@ else:
     print("The test case number will be less than 100")
 
 
+T = int(input("Write the test case number, T =  "))
+if T <= 100:
+    lst = []
+    # n = int(input("Enter number of elements : "))
+    for i in range(0, T):
+        ele = str(input())
+        lst.append(ele)
+    print(lst)
+
+else:
+    print("The test case number will be less than 100")
+
+
+
+    
+    T = int(input("Write the test case number, T =  "))
+if T <= 100:
+    lst = []
+    # n = int(input("Enter number of elements : "))
+    for i in range(0, T):
+        ele = str(input())
+        lst.append(ele)
+    print(lst)
+
+else:
+    print("The test case number will be less than 100")
+
+
