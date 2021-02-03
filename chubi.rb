26e1fde1602eed159f65d40efe9575fd4e4cb999
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-02-03 23:04:34 +0600

    Update chubi.rb

--- chubi.rb
+++ chubi.rb
@@ -13436,59 +13436,7 @@ sclass Reverse:
 >>> iter(rev)
 <__main__.Reverse object at 0x00A1DB50>
 >>> for char in rev:
-...     print(char)
-...
-m
-a
-p
-sclass Reverse:
-    """Iterator for looping over a sequence backwards."""
-    def __init__(self, data):
-        self.data = data
-        self.index = len(data)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index == 0:
-            raise StopIteration
-        self.index = self.index - 1
-        return self.data[self.index]
->>>
->>> rev = Reverse('spam')
->>> iter(rev)
-<__main__.Reverse object at 0x00A1DB50>
->>> for char in rev:
-...     print(char)
-...
-m
-a
-p
-sclass Reverse:
-    """Iterator for looping over a sequence backwards."""
-    def __init__(self, data):
-        self.data = data
-        self.index = len(data)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index == 0:
-            raise StopIteration
-        self.index = self.index - 1
-        return self.data[self.index]
->>>
->>> rev = Reverse('spam')
->>> iter(rev)
-<__main__.Reverse object at 0x00A1DB50>
->>> for char in rev:
-...     print(char)
-...
-m
-a
-p
+.
 sclass Reverse:
     """Iterator for looping over a sequence backwards."""
     def __init__(self, data):
