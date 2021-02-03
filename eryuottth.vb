90f30afcf8e0ae63b8432407a412065d407210e0
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-02-03 23:00:28 +0600

    Update eryuottth.vb

--- eryuottth.vb
+++ eryuottth.vb
@@ -12,21 +12,7 @@ var filePath = Path.Combine("wwwroot", "js", "site.js");
 var fileInfo = provider.GetFileInfo(filePath);
 Types in the preceding example:
 
-provider is an IFileProvider.
-contents is an IDirectoryContents.
-fileInfo is an IFileInfo.var provider = new PhysicalFileProvider(applicationRoot);
-var contents = provider.GetDirectoryContents(string.Empty);
-var filePath = Path.Combine("wwwroot", "js", "site.js");
-var fileInfo = provider.GetFileInfo(filePath);
-Types in the preceding example:
 
-provider is an IFileProvider.
-contents is an IDirectoryContents.
-fileInfo is an IFileInfo.var provider = new PhysicalFileProvider(applicationRoot);
-var contents = provider.GetDirectoryContents(string.Empty);
-var filePath = Path.Combine("wwwroot", "js", "site.js");
-var fileInfo = provider.GetFileInfo(filePath);
-Types in the preceding example:
 
 provider is an IFileProvider.
 contents is an IDirectoryContents.
