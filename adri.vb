13bba08634e368405a298fb0acd592e2b8628aa5
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-02-11 22:05:22 +0600

    Update adri.vb

--- adri.vb
+++ adri.vb
@@ -43,6 +43,19 @@ testStr = Format(testDateTime, "HH:mm:ss")
 ' Returns "23".
 testStr = Format(23)
 
+
+' Returns the value of testDateTime in user-defined date/time formats.
+' Returns "5:4:23".
+testStr = Format(testDateTime, "h:m:s")
+' Returns "05:04:23 PM".
+testStr = Format(testDateTime, "hh:mm:ss tt")
+' Returns "Saturday, Jan 27 2001".
+testStr = Format(testDateTime, "dddd, MMM d yyyy")
+' Returns "17:04:23".
+testStr = Format(testDateTime, "HH:mm:ss")
+' Returns "23".
+testStr = Format(23)
+
 ' Returns the value of testDateTime in user-defined date/time formats.
 ' Returns "5:4:23".
 testStr = Format(testDateTime, "h:m:s")
